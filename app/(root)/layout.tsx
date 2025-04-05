598c86b5315d032dd7937faf512c69bcd1d94c67
yash-sans2004 <nayan251198@gmail.com>
2025-04-05 08:03:46 +0530

    Create a home page

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -11,7 +11,7 @@ const Layout = async ({ children }: { children: ReactNode }) => {
       <nav>
         <Link href="/" className="flex items-center gap-2">
           <Image src="/logo.svg" alt="MockMate Logo" width={38} height={32} />
-          <h2 className="text-primary-100">Nirikshak</h2>
+          <h2 className="text-primary-100">निरीक्षक</h2>
         </Link>
       </nav>
 
--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -59,7 +59,7 @@ const AuthForm = ({type} : {type: FormType}) => {
       <div className="flex flex-col gap-6 card py-14 px-10">
         <div className="flex flex-row gap-2 justify-center">
           <Image src="/logo.svg" alt="logo" height={32} width={38} />
-          <h2 className="text-primary-100">Nirikshak</h2>
+          <h2 className="text-primary-100">निरीक्षक</h2>
         </div>
 
         <h3>Practice job interviews with AI</h3>
